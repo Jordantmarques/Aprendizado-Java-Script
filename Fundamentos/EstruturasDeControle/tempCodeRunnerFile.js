6dbeb7a937fea41290cfda86e55d92278ac7ca3b
Jordantmarques <87248060+Jordantmarques@users.noreply.github.com>
2024-01-04 16:42:23 -0300

    desafio_if_else

--- Fundamentos/EstruturasDeControle/tempCodeRunnerFile.js
+++ Fundamentos/EstruturasDeControle/tempCodeRunnerFile.js
@@ -1,31 +1,31 @@
-// Declaração das variáveis
-var nota;
-var conceito;
+console.log('\n');
 
-// Atribuição das variáveis
-nota = 9.8;
+// Declaração de variáveis
+let a;
+let b;
+let operacao = '%';
+let resultado;
+
+// Atribuição de variáveis
+a = 10;
+b = 28;
 
 // Regras de negócio
-if(nota >= 9 && nota <=10)
-{
-    let conceito = 'A';
+if(operacao === '+') {
+    resultado = a + b;
 }
-else if(nota >= 7.0 && nota <= 8.9)
-{
-    conceito = 'B';
+else if(operacao === '-') {
+    resultado = a - b;
 }
-else if(nota >= 5.0 && nota <= 6.9)
-{
-    conceito = 'C';
+else if(operacao === '*') {
+    resultado = a * b;
 }
-else if(nota >= 4.5 && nota <= 4.9)
-{
-    conceito = 'D';
+else if(operacao === '/') {
+    resultado = a / b;
 }
-else if(nota < 4.5)
-{
-    conceito = 'F';
+else if(operacao === '%') {
+    resultado = a % b;
 }
 
-// Impressão de resultados
-console.log(conceito);
+// Impressão de variáveis
+console.log(resultado);
