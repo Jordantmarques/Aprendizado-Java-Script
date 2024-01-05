13d6db5ac815ceda7a28fe0aa892a5673d73592e
Jordantmarques <87248060+Jordantmarques@users.noreply.github.com>
2024-01-05 16:26:30 -0300

    desafio_funcao

---
+++ Fundamentos/Funcoes/desafio_funcao.js
@@ -0,0 +1,19 @@
+function soma(num1, num2) {
+    let resutlado = num1 + num2;
+
+    return resutlado;
+}
+
+function subtrai(num1, num2) {
+    let resutlado = num1 - num2;
+
+    return resutlado;
+}
+
+console.log(soma(3, 4));
+console.log(soma(31, 98));
+
+console.log('\n');
+
+console.log(subtrai(11, 7));
+console.log(subtrai(19, 52));
