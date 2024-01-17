8da59b3d03b42f6552e1897e7df3b7435a167097
Jordantmarques <87248060+Jordantmarques@users.noreply.github.com>
2024-01-16 22:07:06 -0300

    Exercicios_parcial_5

--- Fundamentos/Exercicios/Exercicios.js
+++ Fundamentos/Exercicios/Exercicios.js
@@ -578,3 +578,8 @@ function calcularMedia(vetor) {
 
 calcularMedia([0, 10]);
 calcularMedia([1, 2, 3, 4, 5]);
+
+//----------------------//------------------------------//----------------------------
+    
+console.log('\n');
+
